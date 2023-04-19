60fc0fdf067296bc122450463aee34678d57c79b
Jordan MORIN <j.morin@hitech.fr>
2023-04-19 18:08:14 +0200

    SA KOMPILE PO

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-face-snap',
-  templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+    selector: 'app-face-snap',
+    templateUrl: './face-snap.component.html',
+    styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent {
+    title!: string;
+    description!: string;
+    id!: number;
+    creationDate!: Date;
+    snaps!: number;
 
+    ngOnInit() {
+        this.title = "Le titre est fou";
+        this.description = "mais la description est encore plus folle";
+        this.id = 16;
+        this.creationDate = new Date();
+        this.snaps = 6;
+    }
 }
