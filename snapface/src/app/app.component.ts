c510cb74f10bbc6589b9cb4f2094971a93356dbd
Jordan MORIN <j.morin@hitech.fr>
2023-04-24 15:25:41 +0200

    this is a fast commit

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
                 description: 'Mon meilleur ami depuis tout petit !',
                 imageUrl: 'https://picsum.photos/id/' + 1 + '/200/100',
                 createdDate: new Date(),
-                snaps: 0,
+                snaps: 14,
                 location: 'Toulouse'
             }, {
                 id: 2,
@@ -29,14 +29,14 @@ export class AppComponent implements OnInit {
                 description: 'Un endroit magnifique pour les randonnées.',
                 imageUrl: 'https://picsum.photos/id/' + 2 + '/200/100',
                 createdDate: new Date(),
-                snaps: 0
+                snaps: 1
             }, {
                 id: 3,
                 title: 'Un bon repas',
                 description: 'Mmmh que c\'est bon !',
                 imageUrl: 'https://picsum.photos/id/' + 3 + '/200/100',
                 createdDate: new Date(),
-                snaps: 0,
+                snaps: 21,
                 location: 'un endroit où je sais pas ou c\'est'
             }]
     }
