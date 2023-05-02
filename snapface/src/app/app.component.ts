87db1245f23c415ba75f85cf82dc049c37537047
Jordan MORIN <j.morin@hitech.fr>
2023-05-02 10:24:23 +0200

    transfert all initalisation and display from app.component to face-snap-list.components

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from 'src/app/models/face-snap.model';
 
 @Component({
     selector: 'app-root',
@@ -7,37 +6,8 @@ import { FaceSnap } from 'src/app/models/face-snap.model';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    mySnap!: FaceSnap;
-    myOtherSnap!: FaceSnap;
-    myThirdSnap!: FaceSnap;
-
-    lesSnaps!: FaceSnap[];
 
     ngOnInit() {
-        this.lesSnaps = [
-            {
-                id: 1,
-                title: 'Archibald',
-                description: 'Mon meilleur ami depuis tout petit !',
-                imageUrl: 'https://picsum.photos/id/' + 1 + '/200/100',
-                createdDate: new Date(),
-                snaps: 14,
-                location: 'Toulouse'
-            }, {
-                id: 2,
-                title: 'Three Rock Mountain',
-                description: 'Un endroit magnifique pour les randonnées.',
-                imageUrl: 'https://picsum.photos/id/' + 2 + '/200/100',
-                createdDate: new Date(),
-                snaps: 1
-            }, {
-                id: 3,
-                title: 'Un bon repas',
-                description: 'Mmmh que c\'est bon !',
-                imageUrl: 'https://picsum.photos/id/' + 3 + '/200/100',
-                createdDate: new Date(),
-                snaps: 21,
-                location: 'un endroit où je sais pas ou c\'est'
-            }]
+
     }
 }
--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FaceSnap } from 'src/app/models/face-snap.model';
 
 @Component({
-  selector: 'app-face-snap-list',
-  templateUrl: './face-snap-list.component.html',
-  styleUrls: ['./face-snap-list.component.scss']
+    selector: 'app-face-snap-list',
+    templateUrl: './face-snap-list.component.html',
+    styleUrls: ['./face-snap-list.component.scss']
 })
 export class FaceSnapListComponent {
+
+    lesSnaps!: FaceSnap[];
+
+    ngOnInit() {
+        this.lesSnaps = [
+            {
+                id: 1,
+                title: 'Archibald',
+                description: 'Mon meilleur ami depuis tout petit !',
+                imageUrl: 'https://picsum.photos/id/' + 1 + '/200/100',
+                createdDate: new Date(),
+                snaps: 14,
+                location: 'Toulouse'
+            }, {
+                id: 2,
+                title: 'Three Rock Mountain',
+                description: 'Un endroit magnifique pour les randonnées.',
+                imageUrl: 'https://picsum.photos/id/' + 2 + '/200/100',
+                createdDate: new Date(),
+                snaps: 1
+            }, {
+                id: 3,
+                title: 'Un bon repas',
+                description: 'Mmmh que c\'est bon !',
+                imageUrl: 'https://picsum.photos/id/' + 3 + '/200/100',
+                createdDate: new Date(),
+                snaps: 21,
+                location: 'un endroit où je sais pas ou c\'est'
+            }]
+    }
 
 }
