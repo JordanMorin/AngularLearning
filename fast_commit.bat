17a49206fcf854abd16a5779d70a7a9c4398bdf8
JordanMorin <132084858+JordanMorin@users.noreply.github.com>
2023-05-02 09:14:31 +0200

    Update fast_commit.bat

--- fast_commit.bat
+++ fast_commit.bat
@@ -1,4 +1,4 @@
 git add *
 git commit -m "this is a fast commit"
-git pull https://github.com/MythMega/angularlearning.git
-git push https://github.com/MythMega/angularlearning.git
+git pull https://github.com/JordanMorin/angularlearning.git
+git push https://github.com/JordanMorin/angularlearning.git
