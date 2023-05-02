4509641c38f7d648863f781c372afd9570d9dfa2
Jordan MORIN <j.morin@hitech.fr>
2023-05-02 11:55:51 +0200

    add routes module. Add route for facesnaps list is'facesnaps', so the link to get the facesnaps component is localhost/facesnaps

--- snapface/src/app/app.module.ts
+++ snapface/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -16,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
         HeaderComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
     bootstrap: [AppComponent]
